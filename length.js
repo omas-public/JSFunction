a5cf79212be97ecb9d0cd60306d095cea42dd092
Omas Naohiko <omas@std.it-college.ac.jp>
2025-02-03 17:08:20 +0900

    Update length.js

--- length.js
+++ length.js
@@ -3,7 +3,7 @@
  * @param  {String} s [文字列]
  * @return {Number}   [文字数]
  */
-const length = s => Array.from(s, ((len, _) => len = len + 1, 0))
+const length = s => Array.from(s).reduce((len, _) => len = len + 1, 0))
 
 console.assert(length('') === 0, 'NG');
 console.assert(length('abc') === 3, 'NG');
